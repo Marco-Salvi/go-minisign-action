04c3858d0c7edf5f12f06bf3a715bf1397ba624a
Marco-Salvi <marco.salvi@ingv.it>
2024-03-08 11:05:09 +0100

    Refactor file signing logic and add debug print statements

--- main.go
+++ main.go
@@ -40,13 +40,26 @@ func main() {
 		os.Exit(1)
 	}
 
-	files, err := filepath.Glob("../build/bin/*")
+	// Private key decrypted successfully
+	fmt.Println("Private key decrypted successfully")
+
+	// Get the build directory
+	buildDir := filepath.Join("..", "build", "bin")
+
+	fmt.Println("Looking for files in: ", buildDir)
+
+	// Find all the files in the build/bin directory
+	files, err := filepath.Glob(filepath.Join(buildDir, "*"))
 	if err != nil {
 		fmt.Println("Failed to find files: ", err)
 		os.Exit(1)
 	}
 
+	// Sign the first file found
 	for _, file := range files {
+		fmt.Println("Signing file: ", file)
+
+		// Read the file
 		fileBytes, err := os.ReadFile(file)
 		if err != nil {
 			fmt.Println("Failed to read the file: ", err)
@@ -56,6 +69,8 @@ func main() {
 		// Generate the signature
 		signature := minisign.Sign(privateKey, fileBytes)
 
+		fmt.Println("Signature generated successfully")
+
 		// Write the signature to a file
 		err = os.WriteFile("signature.minisig", signature, 0644)
 		if err != nil {
@@ -63,7 +78,7 @@ func main() {
 			os.Exit(1)
 		}
 
-		fmt.Println("File signed successfully")
+		fmt.Println("Signature written to signature.minisig")
 
 		return
 	}
